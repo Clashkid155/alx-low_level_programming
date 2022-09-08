acec744f245bbc7b23e54e09065fba2db47a76c3
Clashkid155 <frankdiss9@gmail.com>
2022-09-08 16:43:47 -0700

    Just puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+* main - Entry point
+*
+* Return: A string
+*/
+
+int main(void)
+{
+	const char putam[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(putam);
+	return (0);
+}
