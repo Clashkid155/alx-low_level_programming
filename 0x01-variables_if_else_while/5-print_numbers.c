240f30b0566dbd3e498f96f12e7c6819baffb4db
Clashkid155 <frankdiss9@gmail.com>
2022-09-09 19:50:38 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - 0 - 10
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 48; ch <= 57; ch++)
+{
+	putchar(ch);
+}
+	putchar(10);
+	return (0);
+}
