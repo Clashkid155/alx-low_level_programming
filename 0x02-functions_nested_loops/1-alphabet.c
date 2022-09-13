6198bf2cdfef9821368bbdef51c17bc6f4ddb51b
Clashkid155 <frankdiss9@gmail.com>
2022-09-13 13:54:21 -0700

    LowerCASE

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet - Print lowercase alphabet
+*
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
