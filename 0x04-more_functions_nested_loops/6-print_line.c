edb9c8ac29af6a7c49df91a71829d6c5541f133b
Clashkid155 <frankdiss9@gmail.com>
2022-09-16 00:46:45 +0100

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line - Draw a straight line
+*
+* @n: Input
+*/
+
+void print_line(int n)
+{
+	if (n >= 0)
+	{
+		for (; n >= 0; n--)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
