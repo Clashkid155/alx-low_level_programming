e5444e7cc8f73af95bda846cb7607138f515187b
Clashkid155 <frankdiss9@gmail.com>
2022-09-16 02:32:24 +0100

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*  print_diagonal - Draw a diagonal \
+*
+* @n: Input
+*/
+
+void  print_diagonal(int n)
+{
+	int k, j;
+
+	if (n > 0)
+	{
+		for (k = 1; k <= n; k++)
+		{
+			for (j = 1; j < k; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+		_putchar('\n');
+		return;
+	}
+	_putchar('\n');
+}
