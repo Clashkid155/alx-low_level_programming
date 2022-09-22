f7a2a823c03aa4c1e67a2adfb7926a27a09bf2a3
Clashkid155 <frankdiss9@gmail.com>
2022-09-21 21:49:01 -0700

    Fisrt letter upper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+
+#define COMMA 44
+#define SEMICOLON 59
+#define DOT 46
+#define EXCLAMATION 33
+#define QUOTE 34
+#define BRACKETOPEN 40
+#define BRACKETCLOSE 41
+#define PARENTHESESOPEN 123
+#define PARENTHESESCLOSE 125
+#define SPACE 32
+#define NEWLINE 10
+#define TABSPACE 9
+
+/**
+* _strlen - Check string length
+*
+* @s: Input string.
+*
+* Return: string length
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+
+/**
+* cap_string - Make first letter uppercase
+* @s: Input
+*
+* Return: Modified string
+*/
+
+char *cap_string(char *s)
+{
+	int i, temp;
+
+	for (i = 0; i <= _strlen(s); i++)
+	{
+		if (s[i] <= 122 && s[i] >= 97)
+		{
+			temp = s[i - 1];
+			if (temp == SPACE || temp == NEWLINE || temp == TABSPACE || temp == DOT)
+			{
+				s[i] = s[i] - 32;
+			}
+
+		}
+
+	}
+	return (s);
+}
