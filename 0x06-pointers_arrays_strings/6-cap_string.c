7af05c85f8c597643d92f9369c7027046b55547d
Clashkid155 <frankdiss9@gmail.com>
2022-09-21 23:12:20 -0700

    How many trial?

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,63 +1,37 @@
 #include "main.h"
-
-
-#define COMMA 44
-#define SEMICOLON 59
-#define DOT 46
-#define QUESTIONmARK 63
-#define EXCLAMATION 33
-#define QUOTE 34
-#define BRACKETOPEN 40
-#define BRACKETCLOSE 41
-#define PARENTHESESOPEN 123
-#define PARENTHESESCLOSE 125
-#define SPACE 32
-#define NEWLINE 10
-#define TABSPACE 9
-
 /**
-* _strlen - Check string length
-*
-* @s: Input string.
-*
-* Return: string length
-*/
-
-int _strlen(char *s)
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
 {
-	int i;
+	int index = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
-}
-
-
-/**
-* cap_string - Make first letter uppercase
-* @s: Input
-*
-* Return: Modified string
-*/
-
-char *cap_string(char *s)
-{
-	int i, temp, d;
-	int sep[13] = {COMMA, SEMICOLON, DOT, EXCLAMATION, QUESTIONmARK,
-	QUOTE, BRACKETOPEN, BRACKETCLOSE, PARENTHESESOPEN,
-	PARENTHESESCLOSE, SPACE, NEWLINE, TABSPACE};
-
-	for (i = 0; i < _strlen(s); i++)
+	while (str[index])
 	{
-		if (s[i] <= 122 && s[i] >= 97)
-		{
-			temp = s[i - 1];
-			for (d = 0; d < 13; d++)
-			{
-				if (temp == sep[d])
-					s[i] = s[i] - 32;
-			}
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	return (s);
+
+	return (str);
 }
