20ca729e3949f58c09f290186f300dcea665eefc
Clashkid155 <frankdiss9@gmail.com>
2022-10-10 14:58:05 -0700

    Na dog o

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Just a dog
+* @name: Ask the dog
+* @age: Same with above
+* @owner: Geezz!!
+*
+* Description: Go and ask your grandfather.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
