f0b4525917c45a30389fe0e2ed16e0b894071b4b
Clashkid155 <frankdiss9@gmail.com>
2022-10-25 06:33:23 -0700

    Free a pointer linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* free_listint2 - Free the node
+* @head: The node/list
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	while (*head)
+	{
+		new = (*head)->next;
+		free(*head);
+		*head = new;
+	}
+	head = NULL;
+}
